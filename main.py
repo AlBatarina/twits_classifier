2c12b917cd9f6451b8f6cfe769d489b8f9720871
Batarina <shurochka@yandex.ru>
2015-04-16 13:09:27 +0300

    update

--- main.py
+++ main.py
@@ -23,15 +23,16 @@ import random as rnd
 
 def get_user_tweets(user_id):
     """returns list of tweets as dicts"""
-    try:
-        statuses = api.GetUserTimeline(user_id, count=200)
-    except:
-        waitTime = api.GetSleepTime("statuses/user_timeline")
-        if waitTime == 0:
-            return None
-        print "Waiting ", waitTime, "seconds"
-        time.sleep(waitTime)
-        statuses = api.GetUserTimeline(user_id, count=200)
+    while True:
+        try:
+            statuses = api.GetUserTimeline(user_id, count=200)
+            break
+        except:
+            waitTime = api.GetSleepTime("statuses/user_timeline")
+            if waitTime == 0:
+                return None
+            print "Waiting ", waitTime, "seconds"
+            time.sleep(waitTime)
     dictionaries = []
     for i in range(0,len(statuses)):
         dict = statuses[i].AsDict() #statuses[i].GetRetweeted()
@@ -69,7 +70,7 @@ def get_tokens(words):
         words[i] = re.sub(ur"\W", "", words[i], flags=re.U)
         wnl.lemmatize(words[i])
     stpwrd = stopwords.words('english')
-    stpwrd.extend(['m','re','o','d','vs','w','3','2','rt'])
+    stpwrd.extend(['m','re','o','d','vs','w','3','2','rt','u','ll'])
     tokens = [i for i in words if i not in stpwrd]
     #print tokens
     return tokens
@@ -79,6 +80,7 @@ def get_tweet_tokens(tweet):
 
 def collect_users_tokens(df_users):
     """returns users list and list of user dicts. Each dict contains frequence of user tokens"""
+
     print "Collecting data.."
     users_tweets = []
     users = []
@@ -93,12 +95,18 @@ def collect_users_tokens(df_users):
     tweets = open("./tweets", 'w')
     pck.dump(users_tweets,tweets)
     tweets.close()
+    usrs = open("./users", 'w')
+    pck.dump(users,usrs)
+    usrs.close()
     '''
     print "Opening dictionaries.."
-    raw_input()
+    #raw_input()
     tweets = open("./tweets", 'r')
     users_tweets = pck.load(tweets)
     tweets.close()
+    usrs = open("./users", 'r')
+    users = pck.load(usrs)
+    usrs.close()
     '''
     users_tokens = []
     #nltk.download()
@@ -114,7 +122,7 @@ def collect_users_tokens(df_users):
 TRAINING_SET_URL = "twitter_train.txt"
 df_users = pd.read_csv(TRAINING_SET_URL, sep=",", header=1, names=["user_id", "class"])
 print df_users.head()
-df_users = df_users[:300]
+#df_users = df_users[:300]
 
 print "Authentication.."
 
@@ -155,9 +163,9 @@ def draw_tag_cloud(v, vs):
         size = 30/((maxFreq-tokenFrequency[indexPopular[i]])/(maxFreq-minFreq)+1)
         n = 295
         alpha = n*np.pi/tagsNumb*(tagsNumb-i)
-        r = np.log(np.log(float(tagsNumb-i)/tagsNumb/2 + 1) + 1) + 0.13
+        r = np.log(np.log(float(tagsNumb-i)/tagsNumb/2 + 1) + 1)*0.9 + 0.14
         print 'alpha', alpha
-        pl.text(0.5+r*np.cos(alpha), 0.5+r*np.sin(alpha), v.get_feature_names()[indexPopular[i]],
+        pl.text(0.46+r*np.cos(alpha), 0.5+r*np.sin(alpha), v.get_feature_names()[indexPopular[i]],
                 {'color' : colors[i%len(colors)], 'fontsize' : size} )
     pl.show()
     return
